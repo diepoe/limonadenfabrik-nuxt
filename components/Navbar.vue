50ed0fa2696cb0853717d6caadf1f7b0d32d1c8e
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2021-10-16 11:38:46 +0200

    remove unnecessary type checking

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -28,87 +28,86 @@
       </li>
     </ul>
     <!--Mobile nav-->
-  <div class="grid col-span-1 mr-4 lg:hidden place-items-end">
-    <button
-      class="p-2 rounded hover:bg-blue-100 dark:hover:bg-bluegray-800"
-      aria-label="show menu"
-      @click="toggleMenu(true)"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="stroke-current text-bluegray-800 dark:text-bluegray-200"
-        width="24"
-        height="24"
-        stroke-width="2"
-        stroke="currentColor"
-        fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        ><path d="M0 0h24v24H0z" stroke="none" /><path
-          d="M4 6h16M4 12h16M4 18h16"
-        /></svg
+    <div class="grid col-span-1 mr-4 lg:hidden place-items-end">
+      <button
+        class="p-2 rounded hover:bg-blue-100 dark:hover:bg-bluegray-800"
+        aria-label="show menu"
+        @click="toggleMenu(true)"
       >
-    </button>
-  </div>
-  <div :class="{hidden:!showMenu}">
-    <div
-      class="absolute inset-x-0 top-0 z-40 p-2 transition origin-top-right transform lg:hidden"
-    >
-      <div
-        class="overflow-hidden rounded-lg shadow-md bg-blue-50 dark:bg-bluegray-800"
-      >
-        <div class="px-3 pt-4">
-          <div class="-mr-2">
-            <button
-              type="button"
-              @click="toggleMenu(false)"
-              aria-label="Toggle mobile menu"
-              class="inline-flex items-center justify-center p-2 text-red-500 rounded-md dark:hover:bg-bluegray-700 hover:text-red-600 hover:bg-blue-100 focus:outline-none"
-            >
-              <span class="sr-only">Close main menu</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="w-6 h-6 md:w-7 md:h-7"
-                width="24"
-                height="24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                aria-hidden="true"
-                ><path d="M0 0h24v24H0z" stroke="none" /><path
-                  d="M18 6 6 18M6 6l12 12"
-                /></svg
-              >
-            </button>
-          </div>
-        </div>
-        <div
-          role="menu"
-          aria-orientation="vertical"
-          aria-labelledby="main-menu"
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="stroke-current text-bluegray-800 dark:text-bluegray-200"
+          width="24"
+          height="24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         >
-          <div class="px-4 pt-2 pb-3 space-y-2 md:px-8" role="none">
-      
+          <path d="M0 0h24v24H0z" stroke="none" />
+          <path d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </button>
+    </div>
+    <div :class="{ hidden: !showMenu }">
+      <div
+        class="absolute inset-x-0 top-0 z-40 p-2 transition origin-top-right transform lg:hidden"
+      >
+        <div
+          class="overflow-hidden rounded-lg shadow-md bg-blue-50 dark:bg-bluegray-800"
+        >
+          <div class="px-3 pt-4">
+            <div class="-mr-2">
+              <button
+                type="button"
+                @click="toggleMenu(false)"
+                aria-label="Toggle mobile menu"
+                class="inline-flex items-center justify-center p-2 text-red-500 rounded-md dark:hover:bg-bluegray-700 hover:text-red-600 hover:bg-blue-100 focus:outline-none"
+              >
+                <span class="sr-only">Close main menu</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-6 h-6 md:w-7 md:h-7"
+                  width="24"
+                  height="24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  aria-hidden="true"
+                >
+                  <path d="M0 0h24v24H0z" stroke="none" />
+                  <path d="M18 6 6 18M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="main-menu"
+          >
+            <div class="px-4 pt-2 pb-3 space-y-2 md:px-8" role="none">
               <NuxtLink
                 rel="prefetch"
                 v-bind:to="item.url"
                 class="block px-3 py-2 text-base font-medium leading-relaxed rounded-md font-kyivsans text-bluegray-700 dark:text-bluegray-300 md:text-lg hover:text-bluegray-900 hover:bg-blue-300"
                 role="menuitem"
                 v-for="item in items"
-        :key="(item.name, item.url)"
-                >{{item.name}}</NuxtLink
+                :key="(item.name, item.url)"
+                >{{ item.name }}</NuxtLink
               >
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
   </nav>
 </template>
 
-<script lang="ts">
+<script>
 export default {
   data() {
     return {
@@ -122,7 +121,7 @@ export default {
     }
   },
   methods: {
-    toggleMenu(state: boolean) {
+      toggleMenu(state) {
       this.showMenu = state
     }
   }
